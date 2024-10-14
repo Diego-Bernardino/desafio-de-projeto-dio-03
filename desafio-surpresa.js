6168394fe451edc920ab2b106aba40703e4dae2e
Diego-Bernardino <diegobernardsouza@hotmail.com>
2024-10-14 12:26:18 -0300

    Implementado um método de saída dinâmico no desafio-surpresa

--- desafio-surpresa.js
+++ desafio-surpresa.js
@@ -11,6 +11,31 @@ class pokemon {
     }
 }
 
+function pokeRegister() {
+    switch (count) {
+        case 0:
+            pokeId001.register();
+            break
+        case 1:
+            pokeId002.register();
+            break
+        case 2:
+            pokeId003.register();
+            break
+        case 3:
+            pokeId004.register();
+            break
+        case 4:
+            pokeId005.register();
+            break
+        case 5:
+            pokeId006.register();
+            break
+        case 6:
+            pokeId007.register();
+    }
+}
+
 let pokeId001 = new pokemon('Poochyena', 2, 'M', 13, 13);
 let pokeId002 = new pokemon('Zigzagoon', 2, 'F', 13, 13);
 let pokeId003 = new pokemon('Dragonite', 5, 'M', 25, 25);
@@ -19,10 +44,7 @@ let pokeId005 = new pokemon('Dragonite', 5, 'F', 24, 24);
 let pokeId006 = new pokemon('Poochyena', 3, 'F', 15, 15);
 let pokeId007 = new pokemon('Wurmple', 2, 'M', 7, 14);
 
-pokeId001.register();
-pokeId002.register();
-pokeId003.register();
-pokeId004.register();
-pokeId005.register();
-pokeId006.register();
-pokeId007.register();
+let count
+for (count = 0; count <= 6; count ++) {
+    pokeRegister();
+}
